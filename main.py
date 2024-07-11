efb7330318e44c6d4c08780b83b777837d36304e
Nikita Dibrov <dibrovnik@yandex.ru>
2024-07-11 23:40:41 +0300

    delete

--- main.py
+++
@@ -1,7 +0,0 @@
-from partition import *
-from visualization import visualize_tree, visualize_tree_with_euler_circles
-
-tree = partition('A', 3, 2)  # Увеличиваем глубину дерева до 2
-
-visualize_tree(tree)
-visualize_tree_with_euler_circles(tree)
